4475b3906c2b25bde02b67d83e632d452907a034
Sajib <p2385553@gmail.com>
2024-01-24 15:14:30 +0600

    upgraded second time

--- FactoryPatternDemo.java
+++ FactoryPatternDemo.java
@@ -2,8 +2,10 @@ public class FactoryPatternDemo {
     public static void main(String[] args) {
         ShapeFactory x1 = ShapeFactory.getInstance();
         ShapeFactory x2 = ShapeFactory.getInstance();
-        Shape shape1 = x1.getShape("SQUARE");shape1.draw();
-        Shape shape2 = x2.getShape("RECTANGLE");shape2.draw();
+        Shape shape1 = x1.getShape("SQUARE");
+        shape1.draw();
+        Shape shape2 = x2.getShape("RECTANGLE");
+        shape2.draw();
         System.out.println("hashcode of shape1: " + x1.hashCode());
         System.out.println("hashcode of shape2: " + x2.hashCode());
     }
